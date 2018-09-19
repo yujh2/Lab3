e4c55ba423ae1de482914c0659ffe733bf8a70c8
Jyh Heng Yu <jyhheng2@illinois.edu>
2018-09-19 17:39:45 -0500

    initial project commit

---
+++ src/testRun.java
@@ -0,0 +1,8 @@
+public class testRun {
+    public static void main(String[] test){
+        for (String calue : test) {
+            System.out.println(calue);
+        }
+        System.out.println("LESGOO");
+    }
+}
