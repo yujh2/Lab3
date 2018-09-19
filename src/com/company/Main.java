5af8388ba830348d6de3a741d8cf8ac4233b3cf9
Jyh Heng Yu <jyhheng2@illinois.edu>
2018-09-19 18:26:46 -0500

    initial project commit UPDATE:FINISHED

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,38 @@
 package com.company;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 
 public class Main {
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
 
     public static void main(String[] args) {
-	// write your code her
-        System.out.println("asd");
+        // write your code her
+        String url = "http://erdani.com/tdpl/hamlet.txt";
+        String script;
+        script = urlToString(url);
+        script = script.toLowerCase();
+        String[] scriptSplit = script.split(" ");
+        int count = 0;
+      //  for (String val : scriptSplit) {
+        //    System.out.println(val);
+        //
+        for (int i = 0; i < scriptSplit.length; i++) {
+            if (scriptSplit[i].contains("prince")) {
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
